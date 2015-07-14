f4d982a5ba8d045e439e01bd455faf555bae3904
zjhiphop <zjhiphop@gmail.com>
2015-07-15 00:35:49 +0800

    1. success page 2. meaninful sum

--- server/server.js
+++ server/server.js
@@ -6,6 +6,33 @@ var InfoModel  = require('./db/info-model');
 DB.connect();
 
 var app = express();
+var trans = {
+    "account": "账户"
+    ,"address": "地址"
+    ,"birthday": "生日"
+    ,"contact": "联系方式"
+    ,"id-number": "身份证号"
+    ,"gender": "性别"
+    ,"file_path": "截图"
+    ,"fb-hour": "全程最佳(时)"
+    ,"fb-minute": "全程最佳(分)"
+    ,"fb-name": "全程最佳(名称)"
+    ,"fb-number": "全程最佳(号码)"
+    ,"fb-second": "全程最佳(秒)"
+    ,"fn-hour": "全程最近(时)"
+    ,"fn-minute": "全程最近(分)"
+    ,"fn-number": "全程最近(号码)"
+    ,"fn-name": "全程最近(秒)"
+    ,"fn-second": "全程最近(秒)"
+    ,"hb-hour": "半程最佳(时)"
+    ,"hb-minute": "半程最佳(分)"
+    ,"hb-number": "半程最佳(号码)"
+    ,"hb-second": "半程最佳(秒)"
+    ,"hn-hour": "全程最近(时)"
+    ,"hn-minute": "全程最近(分)"
+    ,"hn-number": "全程最近(号码)"
+    ,"hn-second": "全程最近(秒)"
+};
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -45,8 +72,27 @@ app.get('/show/list', function(req, res, next) {
             return result;
         });
 
+        var renderData = {
+            list: [],
+            heads: []
+        };
+
+        Object.keys(trans).forEach(function(key) {
+            renderData.heads.push(trans[key]);
+        });
+
+        tplData.forEach(function(item) {
+            var d = [];
+
+            Object.keys(trans).forEach(function(key) {
+                d.push(item[key] || '');
+            });
+
+            renderData.list.push(d);
+        });
+
         res.render('result', {
-            data: tplData
+            data: renderData
         });
     });
 });
@@ -67,11 +113,15 @@ app.post('/', [upload, function(req, res, next) {
 
     var data = JSON.parse(JSON.stringify(req.body));
 
-    data.file_path = '/uploads/' + req.files.files.name;
+    if(req.files.files) {
+        data.file_path = '/uploads/' + req.files.files.name;
+    }
 
     InfoModel.save(data);
 
-    res.send('提交成功');
+    res.set('Content-Type', 'text/html');
+
+    res.sendfile('public/success.html')
 }]);
 
 var server = app.listen(3000, function () {
